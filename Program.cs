8be3b2616ce0edf3f2af510ec3b8a1a2dd9df122
Łukasz Jóźwik <jozwiklukasz@outlook.com>
2019-09-23 23:52:52 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NET.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
